c10f8f73bc26fcd0ace246406d5185bd11ca581b
Marseille <35931388+tionyufa@users.noreply.github.com>
2021-04-01 01:35:23 +0500

    Update StateMachineEnemy.cs

--- Assets/Scriptes/Animator/StateMachineEnemy.cs
+++ Assets/Scriptes/Animator/StateMachineEnemy.cs
@@ -6,10 +6,9 @@ public class StateMachineEnemy : StateMachineBehaviour
     private bool _action;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var s = animator.GetComponent<EnemyGolem>();
-        s.isHitAnimator(_action);
+        var Enemy_Golem = animator.GetComponent<EnemyGolem>();
         _action = true;
-       
+       Enemy_Golem.isHitAnimator(_action);
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
